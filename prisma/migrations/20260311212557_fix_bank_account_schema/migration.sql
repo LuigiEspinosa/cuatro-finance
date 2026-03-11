c24fd8205f28314f82e114be961725437b3253b8
Cuatro <luigi@cuatro.dev>
2026-03-11 16:26:57 -0500

    fix(db): Remove Better Auth OAuth fields from BankAccount model

---
+++ prisma/migrations/20260311212557_fix_bank_account_schema/migration.sql
@@ -0,0 +1,28 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `accessToken` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `accessTokenExpiresAt` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `accountId` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `idToken` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `password` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `providerId` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `refreshToken` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `refreshTokenExpiresAt` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `scope` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `Account` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Account" DROP COLUMN "accessToken",
+DROP COLUMN "accessTokenExpiresAt",
+DROP COLUMN "accountId",
+DROP COLUMN "createdAt",
+DROP COLUMN "idToken",
+DROP COLUMN "password",
+DROP COLUMN "providerId",
+DROP COLUMN "refreshToken",
+DROP COLUMN "refreshTokenExpiresAt",
+DROP COLUMN "scope",
+DROP COLUMN "updatedAt";
